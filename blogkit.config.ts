2d5b7bfdcb4c55d355f69c9a7993139656c93f27
2nthony <ant.hu@outlook.com>
2022-03-03 01:27:44 +0800

    feat: rss

--- blogkit.config.ts
+++ blogkit.config.ts
@@ -5,6 +5,8 @@ import { theme } from "blogkit-theme-minimal";
 export default defineConfig({
   siteConfig: {
     title: "2nthony",
+    author: "2nthony",
+    url: "https://2nthony.com",
   },
   theme,
   request,
